7716575db92e51241bce253f2398ec0afe491712
Gaurav Sablok <gauravcodepro@gmail.com>
2025-01-22 09:07:32 +0100

    sciencegenome
    a single enum for the analysis and with in enum an avl tree for the bivariant

--- src/args.rs
+++ src/args.rs
@@ -15,46 +15,10 @@ pub struct CommandParse {
 pub enum Commands {
     /// search for the specific species in the bacdive to get their strain and associated
     /// information
-    ID {
+    Strain {
      /// please provide the path to the bacdive file
       bacdive_analyzer: String,
      /// please provide the specific id that you want to look
-      id: String,
-    },
-    /// prepare the data for the backhand api integration
-    Species {
-      /// please provide the path for the bacdive file
-      bacdive_analyzer: String,
-      /// please provide the specific species that you want to look
-      species: String,
-    },
-    /// please provide the id of the species that you want to look,
-    Designation {
-        /// please provide the path to the bacdive file
-        bacdive_analyzer: String,
-        /// please provide the designation header that you want to search
-        designation_header: String,
-    },
-    /// please provide the species that need to be searched.
-    Strain {
-        /// please provide the path to the bacdive file
-        bacdive_analyzer: String,
-        /// please provide the strain number that you want to look for.
-        strain: String,
-    },
-    /// please provide the type for the bacdive that you want to look
-    StrainType {
-        /// please provide the path to the bacdive file
-        bacdive_analyzer: String,
-        /// type strain that you want to look for in the bacdive.
-        strain_type: String,
-    },
-    /// please provide the bacdiveid for the link to prepare
-    BacdiveWeb {
-       /// please provide the path to the bacdive file
-       bacdive_analyzer: String,
-       /// this will take a text file with the ids to generate the bacdive links.
-       csv: Option<String>
+      strain: String,
     }
-
 }
